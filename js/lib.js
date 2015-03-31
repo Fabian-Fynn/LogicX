47bef0ddcfa1e359c0620f72f8caa942ee4e07ad
Fabian Johannes Hoffmann <fhoffmann.mmt-b2013@fh-salzburg.ac.at>
2015-03-31 02:18:42 +0200

    feat dragging complete

--- js/lib.js
+++ js/lib.js
@@ -1,5 +1,1 @@
 lib = window.lib || {};
-
-lib.log = function(text) {
-    $('#output')[0].innerHTML += text + '<br/>';
-};
--- js/main.js
+++ js/main.js
@@ -4,82 +4,152 @@ var hintsHor = []
 var hintsVer = [[]]
 var mode = 1
 var currentFilling = 0
-var drag = false
-$('body').append('<div id="winMessage"><div id="message"> <h1>You did it!</h1><button id="newGame">Restart</button> </div></div>');
-$('#winMessage').hide();
-startGame();
+var dragging = false
+var startSlot
+$('body').append('<div id="winMessage"><div id="message"> <h1>You did it!</h1><button id="newGame">Restart</button> </div></div>')
+$('#winMessage').hide()
 
+function getSlot($element) {
+  var id = $element.attr('id')
+  var rows = board.length
+  var c = id % rows
+  var r = Math.floor(id / rows)
+  var status = result[r][c]
 
-function slotClick () {
-
-    var slot = getSlot($(this).attr('id'))
-    if(drag){
-      result[slot.row][slot.col] = currentFilling;
-      drawGame()
-      check()
-    }
-
-    console.log(drag)
-
-
-};
-function getSlot(id) {
-  var rows = board.length;
-  var c = id % rows;
-  var r = Math.floor(id / rows);
-  var status = result[r][c];
-
-  return {state: status, row: r, col: c}
+  return {state: status, row: r, col: c, id: id}
 }
-function clickHandler(event) {
-    drag = true;
-    var slot = getSlot($(this).attr('id'))
-    if(slot.state == mode) {
-      currentFilling = 0
-      console.log("nope")
-    }
-    else {
-      currentFilling = mode
-      console.log("yep")
 
-    }
-    result[slot.row][slot.col] = currentFilling;
-
-    drawGame()
-    check()
-    event.stopPropagation();
+function click() {
+  startSlot = getSlot($(this))
+  if (startSlot.state == 0) {
+    currentFilling = mode
   }
+  else {
+    currentFilling = 0
+  }
+  dragging = true
+}
+
+function drag() {
+  var slot = getSlot($(this))
+  if(dragging){
+    dragSlot(slot, false)
+  }
+}
+
+function endDrag() {
+  var slot = getSlot($(this))
+  if(dragging){
+    dragging = false
+    dragSlot(slot, true)
+  }
+}
+
+function dragLeftSlot(){
+  $(this).removeClass('filled-drag')
+  $(this).removeClass('note-drag')
+}
+
+function dragSlot(slot, ended){
+  var targets = []
+
+  if(slot.row == startSlot.row){
+    //left
+    if(slot.col < startSlot.col) {
+      for (var i = startSlot.col; i >= slot.col; i--) {
+        targets.push({row: slot.row, col: i, id: i + (board.length * slot.row), state: result[slot.row][i]})
+      }
+    }
+    //right
+    else{
+      for (var i = startSlot.col; i <= slot.col; i++) {
+        targets.push({row: slot.row, col: i, id: i + (board.length * slot.row), state: result[slot.row][i]})
+      }
+    }
+  }
+  else if(slot.col == startSlot.col)
+  {
+
+    //up
+    if(slot.row < startSlot.row) {
+
+      for (var i = startSlot.row; i >= slot.row; i--) {
+        targets.push({row: i, col: slot.col, id: (i * board.length) + slot.col, state: result[i][slot.col]})
+      }
+    }
+    //down
+    else{
+      for (var i = startSlot.row; i <= slot.row; i++) {
+        targets.push({row: i, col: slot.col, id: (i * board.length) + slot.col, state: result[i][slot.col]})
+      }
+    }
+  }
+  $.each(targets, function(i, element){
+    if((startSlot.state != 0 && element.state == startSlot.state)){
+      if(ended){
+        result[element.row][element.col] = 0
+        drawGame()
+      }
+      else{
+        $('button#' + element.id).removeClass('filled')
+        $('button#' + element.id).removeClass('note')
+      }
+    }
+    if(startSlot.state == 0 && element.state == 0){
+      if(mode == 1) {
+        if(ended){
+          result[element.row][element.col] = 1
+          $('button#' + element.id).removeClass('filled-drag')
+          drawGame()
+        }
+        else{
+          $('button#' + element.id).addClass('filled-drag')
+        }
+      }
+      else{
+        if(ended){
+          result[element.row][element.col] = 2
+          $('button#' + element.id).removeClass('note-drag')
+          drawGame()
+        }
+        else{
+          $('button#' + element.id).addClass('note-drag')
+        }
+      }
+    }
+  })
+}
+
 $('#mode').click(changeMode)
 $('#newGame').click(startGame)
 
 function startGame() {
-  $('#winMessage').hide();
-  var rows = 8;
-  board = [];
-  result = [];
+  $('#winMessage').hide()
+  var rows = 8
+  board = []
+  result = []
   hintsHor = []
   hintsVer = [[]]
   $('#board').css('width', rows * 50)
 
-  $('#board').empty();
+  $('#board').empty()
 
-  var row = [];
-  var currentCol;
-  var slotCount = 0;
+  var row = []
+  var currentCol
+  var slotCount = 0
   for (var i = 0 ; i < rows; i++) { //rows
     for (var j = 0; j < rows; j++) { //cols
-      row[j] = (i+j) % 2;
-      $('#board').append('<button id="' + slotCount + '" class="slot empty">');
-      slotCount++;
+      row[j] = (i+j) % 2
+      $('#board').append('<button id="' + slotCount + '" class="slot">')
+      slotCount++
       if(j == 1 || j == 5)
       {
         row[j] = 1
       }
-    };
-    board.push(row);
-    row = [];
-
-  };
+    }
+    board.push(row)
+    row = []
+  }
 
   board[3][0] = 0
   board[3][1] = 0
@@ -92,16 +162,14 @@ function startGame() {
 
   result = board.map(
     function(){
-      var arr = [];
+      var arr = []
       for (var i = 0; i < this.length; i++) {
-        arr.push(0);
-      };
-      return arr;
-    }, board);
+        arr.push(0)
+      }
+      return arr
+    }, board)
 
-  calcHints(board);
-  console.log(hintsHor)
-  console.log(hintsVer)
+  calcHints(board)
 
   $('#hintsVertical').css('width', rows * 50 )
   $('#hintsHorizontal').css('margin-top', $('#hintsVertical').outerHeight())
@@ -110,69 +178,78 @@ function startGame() {
 
   $('#game').css('width', $('#board').outerWidth() + $('#hintsHorizontal').outerWidth() + 1)
   $('#game').css('height', $('#board').outerHeight() + $('#hintsVertical').outerHeight() + 1)
-  $('.slot').mousedown(clickHandler)
-  $('.slot').mouseup(function() { drag = false});
-  $('#game').mouseleave(function() { drag = false});
+  $('.slot').mousedown(click)
+  $('.slot').mouseup(endDrag)
+  $('.slot').mouseleave(dragLeftSlot)
+  $('#game').mouseleave(function() { dragging = false})
 
-  $('.slot').mouseover(slotClick)
-  drawGame();
-  var slotCount = 0;
-  // for (var row = 0; row < result.length; row++) {
-  //   for (var col = 0; col < result.length; col++) {
-  //     if(board[row][col] == 1) {
-  //       $('#' + slotCount).addClass('filled');
-  //     }
-  //     slotCount++
-  //   }
-  // }
+  $('.slot').mouseenter(drag)
+  drawGame()
+  var slotCount = 0
+
+}
+
+function solve() {
+  var slotCount = 0
+  for (var row = 0; row < result.length; row++) {
+    for (var col = 0; col < result.length; col++) {
+      if(board[row][col] == 1) {
+        // $('#' + slotCount).addClass('filled')
+        result[row][col] = 1
+      }
+      slotCount++
+    }
+  }
+  drawGame()
+  check()
 }
 
 function drawGame () {
-  var slotCount = 0;
+  var slotCount = 0
   for (var row = 0; row < result.length; row++) {
     for (var col = 0; col < result.length; col++) {
       if(result[row][col] == 1) {
-        $('#' + slotCount).addClass('filled');
+        $('#' + slotCount).addClass('filled')
       }
-      else if(result[row][col] == 2)
-      {
-        $('#' + slotCount).removeClass('filled');
-        $('#' + slotCount).addClass('note');
+      else if(result[row][col] == 2){
+        $('#' + slotCount).removeClass('filled')
+        $('#' + slotCount).addClass('note')
       }
       else {
-        $('#' + slotCount).removeClass('filled');
-        $('#' + slotCount).removeClass('note');
+        $('#' + slotCount).removeClass('filled')
+        $('#' + slotCount).removeClass('note')
       }
-      slotCount++;
-    };
-  };
-  if(mode == 1)
-  {
+      $('#' + slotCount).removeClass('filled-drag')
+      $('#' + slotCount).removeClass('note-drag')
+      slotCount++
+    }
+  }
+  if(mode == 1){
     $('#mode button').css('background-image', 'url("img/pen.png")')
   }
-  else
-  {
+  else{
     $('#mode button').css('background-image', 'url("img/pencil.png")')
   }
+  check()
 }
 
 function check () {
-  var won = true;
+  var won = true
   for (var row = 0; row < result.length; row++) {
     for (var col = 0; col < result.length; col++) {
       if(board[row][col] != result[row][col] && result[row][col] != 2) {
-        won = false;
+        won = false
       }
     }
   }
 
   if(won) {
-    $('#winMessage').show();
+    $('#winMessage').show()
   }
 }
 
 function calcHints(board) {
-  var rowAmount = board.length;
+  var rowAmount = board.length
   var blockHor = 0
   var blocksHor = []
   var blockVer = []
@@ -184,17 +261,14 @@ function calcHints(board) {
     for (var col = 0; col < rowAmount; col++) {
       if(board[row][col] == 1){
         blockHor++
-        if(blockVer[col] != null)
-        {
+        if(blockVer[col] != null){
           blockVer[col]++
         }
         else {
           blockVer[col] = 1
         }
-
       }
-      else if(blockVer[col] == undefined)
-      {
+      else if(blockVer[col] == undefined){
         blockVer[col] = 0
       }
       if((blockHor != 0) && (board[row][col] != 1 || col == rowAmount - 1 )) {
@@ -206,60 +280,50 @@ function calcHints(board) {
         blockVer[col] = 0
       }
 
-    };
-
+    }
     hintsHor.push(blocksHor)
     blocksHor = []
     blockHor = 0
-
-  };
+  }
   //append Hints
   var hor
   var ver
-  $('#hintsHorizontal').empty();
-  $('#hintsVertical').empty();
+  $('#hintsHorizontal').empty()
+  $('#hintsVertical').empty()
   for (var i = 0; i < rowAmount; i++) {
     $('#hintsVertical').append('<div id="ver' + i + '" class="hint vertical"></div>')
     $('#hintsHorizontal').append('<div id="hor' + i + '" class="hint horizontal"></div>')
     for (var j = 0; j < hintsVer[i].length; j++) {
       ver = hintsVer[i][j]
-       if(j < hintsVer[i].length - 1)
-      {
+       if(j < hintsVer[i].length - 1){
         ver += '<br>'
       }
       $('#hintsVertical #ver' + i).append(ver)
-    };
+    }
 
     if(hintsHor[i].length == 0) {
       $('#hintsHorizontal #hor' + i).append(0)
     }
     for (var j = 0; j < hintsHor[i].length; j++) {
       hor = hintsHor[i][j]
-
-
-      if(j < hintsHor[i].length - 1)
-      {
+      if(j < hintsHor[i].length - 1){
         hor += ', '
       }
       $('#hintsHorizontal #hor' + i).append(hor)
-    };
-    console.log('hints')
-
-  };
-
+    }
+  }
 }
-
-
 
 function changeMode ()
 {
-  if(mode == 1)
-  {
+  if(mode == 1){
     mode = 2
   }
-  else
-  {
+  else{
     mode = 1
   }
   drawGame()
 }
+
+
+startGame()
